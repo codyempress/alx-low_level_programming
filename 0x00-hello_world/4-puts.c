91fcb0cdab26840c25e90317b93be844acfc86a2
Cody Empress <valentinekodes@gmail.com>
2022-10-14 02:51:31 -0700

    edits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
-main(void){
-	puts "Programming is like buidling a multi-lingual puzzle.\n";
+/**
+ * main - Entry point
+ * Return: 0 if successful
+ **/
+int main(void){
+	puts("Programming is like buidling a multi-lingual puzzle.\n");
 	return 0;
 }
