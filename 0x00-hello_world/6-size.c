417f5b07e1096abee27355fddb7f045ce15c1638
Cody Empress <valentinekodes@gmail.com>
2022-10-14 08:54:57 -0700

    edit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	long long four = 1234567890;
 	float five = 5.78;
 
-	printf("Size of a char is: %d byte(s)\n", sizeof(one));
+	printf("Size of a char: %d byte(s)\n", sizeof(one));
 	printf("Size of an int: %d byte(s)\n", sizeof(two));
 	printf("Size of a long int: %d bytes(s)\n", sizeof(three));
 	printf("Size of a long long int: %d bytes(s)\n", sizeof(four));
