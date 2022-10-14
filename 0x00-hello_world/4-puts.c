c7dcfc0864d0182adadadc1c3be5f487884f15f3
Cody Empress <valentinekodes@gmail.com>
2022-10-14 02:58:34 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - Entry point
  * Return: 0 if successful
  **/
-int main(void){
-	puts("Programming is like buidling a multi-lingual puzzle.\n");
+int main(void)
+{
+	puts("Programming is like building a multi-lingual puzzle.\n");
 	return 0;
 }
