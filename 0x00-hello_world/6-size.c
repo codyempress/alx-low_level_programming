162744b514546cd96e141d7d32c3a6d93f5c8178
Cody Empress <valentinekodes@gmail.com>
2022-10-14 08:08:00 -0700

    edit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 /**
  * main - entry point
- * return: 0 if successful
  *
+ * Return: 0 if successful
  */
 
 int main(void)
 {
-	printf(sizeof());
+	char one= 's';
+	int two= 4;
+	long three=12345678;
+	long long four=1234567890;
+	float five =5.78;
+
+	printf("Size of a char is: ", sizeof(one), " byte(s)");
+	printf("Size of an int: ",sizeof(two), " byte(s)");
+	printf("Size of a long int: ",sizeof(three)," bytes(s)");
+	printf("Size of a long long int: ",sizeof(four)," bytes(s)");
+	printf("Size of a float: ", sizeof(five)," byte(s)");
 	return (0);
 }
