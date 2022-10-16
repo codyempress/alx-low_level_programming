0b021b93c3bf5d1bf2d8dbb62c64726699d55657
Cody Empress <valentinekodes@gmail.com>
2022-10-16 05:59:08 -0700

    edit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - emtry point
+ *
+ * for - runs a loop to iterate the alphabet
+ *
+ * Return: returns 0 aftre nd of the loop
+ *
+ */
+
+int main (void)
+{
+	int bet,
+	    bet = 'a';
+
+	for (bet <= 'z'; bet != 'e'; bet !='q';)
+	{
+		putchar(bet);
+		bet++;
+	};
+	putchar('\n');
+
+	Return (0);
+}
