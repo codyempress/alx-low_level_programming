a1910a6f2e815e203be9c4dcc8bc7bcb03273e45
Cody Empress <valentinekodes@gmail.com>
2022-10-16 05:50:13 -0700

    edit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * for statement - runs a loop to reiterate the alphabet
+ *
+ * Return: returns 0 at end of program
+ *
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 'a'; a< = 'z'; a++)
+	{
+		putchar(a);
+	};
+	for (b = 'A'; b <= 'Z'; b++)
+	{
+		putchar(b);
+	};
+
+	putchar('\n');
+
+	return (0);
+}
