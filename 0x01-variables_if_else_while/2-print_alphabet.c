9dc2fe6963ee644ad3ea9cfacbb2d8ee131ab3ea
Cody Empress <valentinekodes@gmail.com>
2022-10-16 05:31:34 -0700

    edit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returns 0 if value is true;
+ *
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 'a'; y <= 'z'; y++)
+	{
+		putchar(y);
+	};
+	return (0);
+}
