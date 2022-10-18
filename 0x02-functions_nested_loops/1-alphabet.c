32200c75219055ac751e35bde53cccc621df96f0
Cody Empress <valentinekodes@gmail.com>
2022-10-18 09:20:05 -0700

    edit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "1-main.h"
+
+/*
+ * main - entry poin
+ *
+ * Return :always 0
+ *
+ */
+
+int main()
+{
+	char c[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+	for (i = 0; i < '27'; i++)
+	{
+		_putchar(c[26]);
+	};
+	_putchar('\n');
+	return (0);
+};
