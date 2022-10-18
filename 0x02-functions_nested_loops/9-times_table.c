d1ad0c2d401bf0ea6e958958ec1347ddfa295704
Cody Empress <valentinekodes@gmail.com>
2022-10-18 15:29:34 -0700

    edit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,4 +11,50 @@
 
 void times_table (void)
 {
+	int row, col, prod;
+
+	if (n >= 0) && (n < 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= row; col++)
+			{
+				prod = row * col;
+				if (prod <= 9)
+				{
+					if (col != 0)
+					{
+						_putchar(",");
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar('0' + prod);
+					
+				}
+				else if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 100));
+					_putchar('0' + (prod / 10) % 10);
+					_putchar('0' + (prod % 10));
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 100));
+					_putchar('0' + (prod / 10) % 10);
+					_putchar('0' + (prod % 10));
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+		
+
+
+
 }
