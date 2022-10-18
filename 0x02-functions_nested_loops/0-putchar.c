ffd5042eed2f5eb9b5cdbf3684148d444daa7aaa
Cody Empress <valentinekodes@gmail.com>
2022-10-18 07:20:27 -0700

    edit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returns 0 after the function runs
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
