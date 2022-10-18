d80ac577c4b1fcec70e44551bfe5455f144319be
Cody Empress <valentinekodes@gmail.com>
2022-10-18 13:41:00 -0700

    edit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *abs - check the absolute value;
+* @int - parameter
+*
+* Description: check for absolute values
+*
+* Return: returns value;
+*
+*/
+
+int _abs(int f)
+{
+	g = int abs(f);
+	return (f);
+};
