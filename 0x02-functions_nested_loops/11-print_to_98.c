87759b564eff0f0c67ec734e0a924f4e1acf5ac1
Cody Empress <valentinekodes@gmail.com>
2022-10-18 14:59:58 -0700

    edit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,20 +11,22 @@
 
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n <= 98)
 	{
-		while (n > 98)
-		{
-			printf("%d, ", n--);
+		if (n == 0)
 			printf("%d\n", n);
-		};
+		else
+			printf("%d\n ", n);
+			n = n + 1;
 	}
-	else
+	else if (n > 98)
 	{
-		while(n < 98)
+		while (n >= 98)
 		{
-			printf("%d ", n++);
-			printf("%d\n", n);
+			if( n == 0)
+				printf("%d\n ", n);
+			printf("%d, ", n);
+			n = n - 1;
 		};
 	};
 }
