ba8f2b3e24e21885bbb68f632cacc40a09af40bb
Cody Empress <valentinekodes@gmail.com>
2022-10-19 11:37:39 -0700

    edit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
+ *
  * print_sign - prints out the sign of the number
  * @n: parameter
  *
  * Description: the functions checks and prints the output as per the sign
  *
- * Return : always 0;
+ * Return : 1 and prints '+' if n > 0;
+ * 		0 and prints 0
+ * 		-1 and prints '/'
  *
  */
 
