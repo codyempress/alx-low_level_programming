735b491c83dbf599bd6cbcd04406bd115ab9fcf2
Cody Empress <valentinekodes@gmail.com>
2022-10-19 11:22:57 -0700

    edit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,14 +4,14 @@
  * _isalpha - function checks for any lower or upper case characters
  * @c : parameter
  *
- * Description: function checks if theres a lower or upper case character and returns 0;
+ * Description: function checks character and returns 0;
  *
  * Return: always 0;
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 97  && c <= 122) || ( c >= 65 && c <= 90))
+	if ((c >= 97  && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
