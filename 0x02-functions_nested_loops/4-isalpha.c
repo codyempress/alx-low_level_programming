241abf44cfc181827f201055e3f52089ec7863d3
Cody Empress <valentinekodes@gmail.com>
2022-10-19 11:17:43 -0700

    edit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,12 +11,9 @@
 
 int _isalpha(int c)
 {
-	if (c >= 'a' || c <= 'z' || c >= 'A' || c >= 'Z')
+	if ((c >= 97  && c <= 122) || ( c >= 65 && c <= 90))
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	};
+	return (0);
 }
