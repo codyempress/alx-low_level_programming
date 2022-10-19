f64b2c7c567c1acd1963584a5664a9eb123e2452
Cody Empress <valentinekodes@gmail.com>
2022-10-19 11:32:25 -0700

    edit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar("+");
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar("0");
+		_putchar(48);
 		return (0);
 	}
 	else
 	{
-		_putchar("-1");
+		_putchar(45);
 		return (-1);
-	};
+	}
 }
