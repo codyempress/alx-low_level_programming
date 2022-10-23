77934935cb12604796a7935f47b2d51b529e0aed
Cody Empress <valentinekodes@gmail.com>
2022-10-23 10:37:47 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - mutiplies two digits
+ * @a: digit one
+ * @b: digit two
+ *
+ * Return: returns 0
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (0);
+}
