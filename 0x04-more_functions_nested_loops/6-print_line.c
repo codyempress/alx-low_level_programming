a6edcc4c9ecce3d99bd34cc12eb61be337a97848
Cody Empress <valentinekodes@gmail.com>
2022-10-24 09:07:58 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ *@n: parameter
+ *
+ * Return: always 0
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (n > 0)
+		{
+			_putchar(95);
+			n--;
+		}
+	}
+	_putchar('\n');
+}
