e6816624e9c8ae3a6d9d028a5ec550fb0b3dfa86
Cody Empress <valentinekodes@gmail.com>
2022-10-24 00:48:28 -0700

    edit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,16 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	char i;
 
-	while(i < 10)
+	for (i = '0'; i <= '9'; i++)
 	{
 		if (i == 2 || i == 4)
 		{
 			continue;
 		}
-		_putchar(i + '0')
+
+		_putchar(i);
+	}
+	_putchar('\n');
+}
