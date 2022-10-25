b820be9331f1f3830f66b2dd1f16772d4fc28d0f
Cody Empress <valentinekodes@gmail.com>
2022-10-25 10:31:59 -0700

    edit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value to 98
+ * @*n: pointer
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	int g = 10;
+	n = &g;
+	*n = 98;
+}
