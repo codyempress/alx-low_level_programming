e72a07bb82fc30ffb0292b914f3f0a9ef5ce0c23
Cody Empress <valentinekodes@gmail.com>
2022-10-26 09:40:29 -0700

    edit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,5 +10,17 @@
 
  void print_rev(char *s)
 {
+	int i, count;
 
+	count = 0;
+	for (i = 0; i + 2 <= len; i+=2);
+	{
+		count += 1;
+	}
+	
+	for (; s[count] >= 0; count--)
+	{
+		putchar(s[count]);
+	}
+	putchar('\n');
 }
