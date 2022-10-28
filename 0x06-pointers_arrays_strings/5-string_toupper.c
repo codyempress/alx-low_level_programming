ec30762f23b1a8d4b0178a8e8a4f74f710355d97
Cody Empress <valentinekodes@gmail.com>
2022-10-28 09:54:57 -0700

    edit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @n: pointer
+ * Return: characters
+ */
+
+char *string_toupper(char *n)
+{
+	int m;
+
+	m = 0;
+
+	for (n[m] = 0; n[m] != '\0'; m++)
+	{
+		if (n[m] <= 'z' || n[m] >= 'a')
+		{
+			toupper(n[m]);
+		}
+	}
+	return (n);
+}
