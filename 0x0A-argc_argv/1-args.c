d39194e7dab3bbf48e263c287afff886e62e2289
Cody Empress <valentinekodes@gmail.com>
2022-11-07 02:59:08 -0800

    edt

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,12 @@
 /**
  * main - prints number of args
  * @argc: count
- * 
+ * @argv: array of parameters
+ *
  * Return: 0
  */
 
-int main(int argc)
+int main(int argc, __attribute__((unused))char *argv[])
 {
 	printf("%d\n", argc);
 	return (0);
