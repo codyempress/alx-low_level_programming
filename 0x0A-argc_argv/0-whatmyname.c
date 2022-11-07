684c3166142998df22861bbe9bce3e6b02435722
Cody Empress <valentinekodes@gmail.com>
2022-11-07 02:18:55 -0800

    edit

---
+++ 0x0A-argc_argv/0-whatmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[argc]);
+
+	return (0);
+}
