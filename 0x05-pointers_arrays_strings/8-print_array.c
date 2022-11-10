cdf2091c137e8ef9a3478f5d8bde8ca703f70c7b
Cody Empress <valentinekodes@gmail.com>
2022-11-10 00:08:10 -0800

    edit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints integers in order
+ * @a: array
+ * @n: number of items
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (i == (n - 1))
+	{
+		printf("%d\n", *(a + i));
+	}
+	else
+	{
+		for (i = 0; i < (n - 1); i++)
+		{
+			printf("%d, ", *(a + i));
+		};
+	}
+	printf("\n");
+}
