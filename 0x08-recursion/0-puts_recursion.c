9f2d5c73307babfc98d8a68bff09460bb2856c3c
Cody Empress <valentinekodes@gmail.com>
2022-11-16 01:09:07 -0800

    edit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,16 +12,14 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if (*(s + i) == '\0')
-	{
-		return;
-	}
-	else
+	if (*(s + i) != '\0')
 	{
 		_putchar(*(s + i));
 		i++;
 		_puts_recursion(s + i);
-	};
-
-	_putchar('\n');
+	}
+	else if (*(s + i) == '\0')
+	{
+		_putchar('\n');
+	}
 }
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * factorial - prints factorial of number
+ * @n: integer
+ *
+ * return: 0;
+ */
+
+int factorial(int n)
+{
+}
