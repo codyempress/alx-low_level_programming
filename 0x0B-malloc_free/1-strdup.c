f2656d9b83447682815058a70a2d5a1db756ce81
Cody Empress <valentinekodes@gmail.com>
2022-11-16 06:16:54 -0800

    edit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - returs pointer to malloc memory
+ * @str: string literal
+ *
+ * Return: a pointer to string
+ */
+
+char *_strdup(char *str)
+{
+	int g = strlen(str) + 1;
+
+	char *t = (char *)malloc (g * sizeof(char));
+
+	t = strdup(str);
+
+	if (str == NULL || t == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (str);
+	};
+	printf("\n");
+}
