5e39fbd04d03036807c7c21c99801b1c3a960568
Cody Empress <valentinekodes@gmail.com>
2022-12-04 06:24:16 -0800

    edit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,4 +8,22 @@
  */
 int print_diagonal(int n)
 {
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (n > 0)
+	{
+		while (n > i)
+		{
+			_putchar(' ');
+			i++;
+		}
+
+		_putchar('92');
+		_putchar('\n');
+	}
+	return (0);
 }
