a10896e5998134fa63c63c8c0c89e3261f3a6da5
Cody Empress <valentinekodes@gmail.com>
2023-02-09 02:21:34 -0800

    Edits

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,10 +3,12 @@
 /**
  * factorial - prints factorial of number
  * @n: integer
- *
- * return: 0;
+ * Return: the factorial of the given number
  */
 
 int factorial(int n)
 {
+	if (n == 1)
+		return (1);
+	return (n * factorial(n - 1));
 }
